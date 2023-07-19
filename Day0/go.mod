f94270658187fbcfc600100f27af2285ea381f3c
majjikishore007 <majjikishore031@gmail.com>
2023-07-19 20:49:03 +0530

    add hello world with tdd

---
+++ Day0/go.mod
@@ -0,0 +1,3 @@
+module github.com/majjikihsore007
+
+go 1.20
---
+++ Day0/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+
+func Hello(name string) string {
+	return "Hello, ,"+name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
