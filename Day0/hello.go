798217ea18c3c6ca55554c3d042b795382ee61bb
majjikishore007 <majjikishore031@gmail.com>
2023-07-29 12:26:57 +0530

    Refactor the code by writing meaning full error messages

--- Day0/hello.go
+++ Day0/hello.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
-	return "Hello, ,"+name
+	if(name == "") {
+		name = "world"
+	}
+	return englishHelloPrefix+name
 }
 
 func main() {
--- Day0/hello_test.go
+++ Day0/hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("world")
-	want := "Hello, ,world"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// helper funciton to tell the test suite that this method is a helper
+		// when it fails the line number reported will be in our function call rather than inside our test helper
+		t.Helper() 
+
+		if(got != want) {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
